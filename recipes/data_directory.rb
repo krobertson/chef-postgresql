c08080d6fde1ae868deabd9cd9c9254de2482939
Ken Robertson <ken@apcera.com>
2013-11-14 00:02:10 -0800

    Instrument initial setup of a new slave and better handling for promotion
    This changes the data directory initialization to use pg_basebackup to load the
    data directory from the master when provisioning a new slave. With this,
    provisioning a new slave is done end to end with a single run, and no need to
    manually pull over the data directory.
    
    Also adjusts some configuration settings that can be set on both the master and
    slaves. Keeping these in sync between the two makes the slave better prepped if
    it gets promoted to the master. These include WAL related settings that will
    allow it have slaves without needing its configuraion updated and server
    restarted.

--- recipes/data_directory.rb
+++ recipes/data_directory.rb
@@ -12,14 +12,27 @@ directory node["postgresql"]["data_directory"] do
   not_if { ::File.exist?("#{node["postgresql"]["data_directory"]}/PG_VERSION") }
 end
 
-# initialize the data directory if necessary
-bash "postgresql initdb" do
-  user "postgres"
-  code <<-EOC
-  /usr/lib/postgresql/#{node["postgresql"]["version"]}/bin/initdb \
-    #{node["postgresql"]["initdb_options"]} \
-    -U postgres \
-    -D #{node["postgresql"]["data_directory"]}
-  EOC
-  not_if { ::File.exist?("#{node["postgresql"]["data_directory"]}/PG_VERSION") }
+# initialize the data directory if necessary and if we are not a slave
+unless node[:postgresql][:slave]
+  bash "postgresql initdb" do
+    user "postgres"
+    code <<-EOC
+    /usr/lib/postgresql/#{node["postgresql"]["version"]}/bin/initdb \
+      #{node["postgresql"]["initdb_options"]} \
+      -U postgres \
+      -D #{node["postgresql"]["data_directory"]}
+    EOC
+    not_if { ::File.exist?("#{node["postgresql"]["data_directory"]}/PG_VERSION") }
+  end
+else
+  bash "postgresql initial data directory from master" do
+    user "postgres"
+    code <<-EOC
+    /usr/lib/postgresql/#{node["postgresql"]["version"]}/bin/pg_basebackup \
+      -h #{node[:postgresql][:primary_conninfo].scan(/host=(\S+)/).flatten.first} \
+      -D #{node["postgresql"]["data_directory"]} \
+      --xlog --checkpoint=fast --progress
+    EOC
+    not_if { ::File.exist?("#{node["postgresql"]["data_directory"]}/PG_VERSION") }
+  end
 end
